f1e948e6404270877102dd8f995544ff5364b197
Yaroslav <0713titov@gmail.com>
2025-03-31 20:02:44 +0300

    test change

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -24,6 +24,9 @@ import { FC } from "react";
                 <Link href="#" color={"inherit"} underline="none">PAGES</Link>
                 <Link href="#" color={"inherit"} underline="none">OUR OFFER</Link>
                 <Link href="#" color={"inherit"} underline="none">PRICING</Link>
+                <Link href="#" color={"inherit"} underline="none">PRICING</Link>
+                <Link href="#" color={"inherit"} underline="none">PRICING</Link>
+                <Link href="#" color={"inherit"} underline="none">PRICING</Link>
         </Box>
                 <OrderButton>Order Now</OrderButton>
                 
